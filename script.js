abfa8c39edb2cd804c02ae6648b5b7ffb8576f49
igralex <igralex1@gmail.com>
2018-12-05 11:26:49 +0300

    add navigate bar

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+// navigate
+    document.getElementById('nav').onmouseover = function (event)   {
+        var target = event.target;
+             if (target.className == 'menu-item'){
+            var s = target.getElementsByClassName('submenu');
+            closeMenu();
+            s[0].style.display='block';
+        }
+    }
+
+document.onmouseover = function (event){
+    var target = event.target;
+    console.log(event.target);
+    if(target.className != 'menu-item' && target.className!= 'submenu'){
+        closeMenu();
+    }
+}
+
+function closeMenu(){
+    var menu  = document.getElementById('nav')
+    var subm = document.getElementsByClassName('submenu');
+    for (var i = 0; i < subm.length; i++){
+        subm[i].style.display = "none";
+    }
+}
+
+
+
+// for RangeBorderRadius
+function funcRngBorderRadius(){
+    var rLU = document.getElementById('radiusLeftUp').value;
+    var rRU = document.getElementById('radiusRightUp').value;
+    var rLD = document.getElementById('radiusLeftDown').value;
+    var rRD = document.getElementById('radiusRightDown').value;
+    var tRLU = document.getElementById('textRadiusLeftUp');
+    var tRRU = document.getElementById('textRadiusRightUp');
+    var tRLD = document.getElementById('textRadiusLeftDown');
+    var tRRD = document.getElementById('textRadiusRightDown');
+
+    var figura = document.getElementById('b');
+
+    tRLD.value = rLD;
+    tRLU.value = rLU;
+    tRRD.value = rRD;
+    tRRU.value = rRU;
+
+    // Изменеие прямоугольника
+    b.style.borderRadius = rLU + 'px '+ rRU + 'px ' + rLD + 'px ' + rRD + 'px ';
+}
+
 //function for Range
 function funRange(){
     var range = document.getElementById('range');
